a19a0cc65c9a3976106e325a161e2195b1366c52
David <david-27@hotmail.com>
2023-08-16 22:01:53 -1100

    feat: fix the iframe

---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+document.getElementById("autentica").addEventListener('click', function(event) {
+    // Prevent the default action (navigation) if needed
+    event.preventDefault();
+    function iframe() {
+        console.log(document.getElementById("myIframe").src);
+    }
+    
+});
+
+
+
+
